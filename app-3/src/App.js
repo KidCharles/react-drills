8ceb9beefcc5fde3912ab95ad50ce01ffa033d92
Charles Hatch <charlesghatch@gmail.com>
2018-09-06 21:47:06 -0700

    completed react drill 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,21 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+// export default class App extends Component{
+//   constructor(){
+//     super()
+//     this.state={
+//       list:['mango', 'poppy', 'rumble', 'Granita', 'GranBob'],
+//       input:''
+//     }
+//   }
+//   handleInput=(val)=> {
+//     this.setState({input:val})
+//   }
+//   render(){
+//     let listToDisplay=this.state.list.filter((e,i)=> {
+//       return e.includes(this.state.input)
+//     }).map((e,i)=> {
+//       return(
+//         <h4 key={i}>{e}</h4>
+//       )
+//     })
+//     return(
+//       <div>
+//         list
+//         <input value={this.state.input} onChange={(e)=> this.handleInput(e.target.value)}/>
+//         {listToDisplay}
+//       </div> 
+//     )
+//   }
+// }
+
+
+import React, {Component} from 'react';
+
+export default class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      sayians:['Kakorot', 'Prince Vageta', 'Gohan', 'Trunks'],
+      name:''
+    }
+  }
+  searchName=(val)=> {
+    this.setState({name:val})
+  }
+  render(){
+    let {sayians, name}=this.state;
+    let mappedSayians=sayians.filter((e,i)=> {
+      return e.includes(name)
+    }).map((e,i)=> {
+      return(
+        <h4 key={i}>{e}</h4>
+      )
+    })
+    return(
+      <div>
+        <input value={this.state.name} onChange={(e)=> this.searchName(e.target.value)}/>
+        {mappedSayians}
+      </div> 
+    )
   }
 }
-
-export default App;
