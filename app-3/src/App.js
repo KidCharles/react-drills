ea79bac8ca4cec8640c03ca66b4f3812e3bf2e0d
Charles Hatch <charlesghatch@gmail.com>
2018-09-07 07:32:54 -0700

    practice again

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -24,39 +24,73 @@
 //         list
 //         <input value={this.state.input} onChange={(e)=> this.handleInput(e.target.value)}/>
 //         {listToDisplay}
-//       </div> 
+//       </div>
 //     )
 //   }
 // }
 
+// import React, {Component} from 'react';
 
-import React, {Component} from 'react';
+// export default class App extends Component{
+//   constructor(){
+//     super()
+//     this.state={
+//       sayians:['Kakorot', 'Prince Vageta', 'Gohan', 'Trunks'],
+//       name:''
+//     }
+//   }
+//   searchName=(val)=> {
+//     this.setState({name:val})
+//   }
+//   render(){
+//     let {sayians, name}=this.state;
+//     let mappedSayians=sayians.filter((e,i)=> {
+//       return e.includes(name)
+//     }).map((e,i)=> {
+//       return(
+//         <h4 key={i}>{e}</h4>
+//       )
+//     })
+//     return(
+//       <div>
+//         <input value={this.state.name} onChange={(e)=> this.searchName(e.target.value)}/>
+//         {mappedSayians}
+//       </div>
+//     )
+//   }
+// }
 
-export default class App extends Component{
-  constructor(){
-    super()
-    this.state={
-      sayians:['Kakorot', 'Prince Vageta', 'Gohan', 'Trunks'],
-      name:''
-    }
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      djs: ["Rl Grime", "Diplo", "yehMe2", "Skrillex", "Baauer"],
+      name: ""
+    };
   }
-  searchName=(val)=> {
-    this.setState({name:val})
-  }
-  render(){
-    let {sayians, name}=this.state;
-    let mappedSayians=sayians.filter((e,i)=> {
-      return e.includes(name)
-    }).map((e,i)=> {
-      return(
-        <h4 key={i}>{e}</h4>
-      )
-    })
-    return(
+  handleSearch = val => {
+    this.setState({ name: val });
+  };
+  render() {
+    let { djs, name } = this.state;
+    let mappedDjs = djs
+      .filter((e, i) => {
+        return e.includes(name);
+      })
+      .map((e, i) => {
+        return <h4 key={i}>{e}</h4>;
+      });
+    return (
       <div>
-        <input value={this.state.name} onChange={(e)=> this.searchName(e.target.value)}/>
-        {mappedSayians}
-      </div> 
-    )
+        <input
+          value={this.state.name}
+          placeholder="search Dj's"
+          onChange={e => this.handleSearch(e.target.value)}
+        />
+        {mappedDjs}
+      </div>
+    );
   }
 }
