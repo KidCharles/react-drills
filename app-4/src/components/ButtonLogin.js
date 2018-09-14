e29f60130d67e33dc19d30be4715e93c920a5f54
Charles Hatch <charlesghatch@gmail.com>
2018-09-13 18:03:21 -0700

    practice complete

--- app-4/src/components/ButtonLogin.js
+++ app-4/src/components/ButtonLogin.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 export default function ButtonLogin(props){
-    console.log(props)
-    return(
-      <button 
-      onClick={props.login}
-      >
-          Login
-      </button>
+    return (
+        <button onClick={props.fn}>Login</button>
     )
 }
--- app-4/src/components/Password.js
+++ app-4/src/components/Password.js
@@ -1,33 +1,38 @@
-import React, {Component} from 'react';
-import Button from './ButtonLogin';
+import React, { Component } from "react";
+import Button from "./ButtonLogin";
 
-export default class Password extends Component{
-    constructor(){
-        super()
-        this.state={
-            username:'',
-            password:''
-        }
-    }
-   handleName=(val)=> {
-        this.setState({username:val})
-    }
-    handlePass=(val)=> {
-        this.setState({password:val})
-    }
-    handleLogin=()=> {
-       alert(`Login : ${this.state.username} with password: ${this.state.password}`)
-    }
-    render(){
-        // let {username, password}=this.state
-        return(
-            <div>
-                <input placeholder="username" value={this.state.username} onChange={(e)=> this.handleName(e.target.value)}/>
-                <input placeholder="password" value={this.state.password} onChange={(e)=> this.handlePass(e.target.value)}/>
-                <Button 
-                login={this.handleLogin}
-                />
-            </div> 
-        )
-    }
+export default class Password extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsername=(val)=> {
+      this.setState({
+          username:val
+      })
+  }
+  handlePassword=(val)=> {
+    this.setState({
+        password:val
+    })
+  }
+
+  handleClick = () => {
+    alert(
+      `logged ${this.state.username} with password : ${this.state.password}`
+    );
+  };
+  render() {
+    return (
+      <div>
+        <input type="" className="" onChange={(e)=> this.handleUsername(e.target.value)} value={this.state.username} />
+        <input type="" className="" onChange={(e)=> this.handlePassword(e.target.value)} value={this.state.password} />
+        <Button fn={this.handleClick} />
+      </div>
+    );
+  }
 }
